31a225e9b41a06eb2ce656bf86e9a3c1db4b7f1d
Kailas Mahavarkar <66670953+KailasMahavarkar@users.noreply.github.com>
2024-08-24 13:39:18 +0530

    updated code

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ import random
 from pygame.rect import *
 from color import *
 
+WIDTH, HEIGHT = 800, 350
+DINO_START_X, DINO_START_Y = 100, 200
+CACTUS_START_X = 500
+
 pygame.init()
 clock = pygame.time.Clock()
 
@@ -19,13 +23,15 @@ pygame.display.set_icon(icon)
 
 
 class Main:
-    def __init__(self):
+    def __init__(self, adjustFrameFactor=2):
         self.run = True
         self.gameover = False
-        self.width = 800
-        self.height = 350
+        self.width = WIDTH
+        self.height = HEIGHT
         self.score = 0
+        self.adjustFrameFactor = adjustFrameFactor
         self.screen = pygame.display.set_mode((self.width, self.height))
+        self.clock = pygame.time.Clock()
 
         # load image as Path
         self.dinoImagePath = os.path.join(testPath, 'dinosaur_64.png')
@@ -34,42 +40,41 @@ class Main:
 
         # set Images
         self.dinoImage = pygame.image.load(self.dinoImagePath).convert_alpha()
-        self.cactusImage = pygame.image.load(self.cactusImagePath).convert_alpha()
-        self.backgroundImage = pygame.image.load(self.backgroundPath).convert_alpha()
+        self.cactusImage = pygame.image.load(
+            self.cactusImagePath).convert_alpha()
+        self.backgroundImage = pygame.image.load(self.backgroundPath).convert()
 
         # set Dino attributes
         dinosaur = Image.open(self.dinoImagePath)
         dinoW, dinoH = dinosaur.size
-        self.dinoX = round(100)
-        self.dinoY = round(self.height - dinoH)
+        self.dinoX = DINO_START_X
+        self.dinoY = self.height - dinoH
 
         # set Cactus attributes
-        cactus = Image.open(self.dinoImagePath)
+        cactus = Image.open(self.cactusImagePath)
         cactusW, cactusH = cactus.size
-        self.cactusX = round(500)
-        self.cactusY = round(self.height - cactusH)
+        self.cactusX = CACTUS_START_X
+        self.cactusY = self.height - cactusH
 
     @property
     def dinosaur(self):
-        self.screen.blit(self.dinoImage, (round(self.dinoX), round(self.dinoY)))
-        hitboxDino = Rect(int((self.dinoX - 5)), int((self.dinoY - 5)), 70, 70)
+        self.screen.blit(self.dinoImage, (self.dinoX, self.dinoY))
+        hitboxDino = self.dinoImage.get_rect(topleft=(self.dinoX, self.dinoY))
         pygame.draw.rect(self.screen, getRGB("red"), hitboxDino, 2)
         return hitboxDino
 
     def cactus(self):
-        self.screen.blit(self.cactusImage, (round(self.cactusX), round(self.cactusY)))
-        hitboxCactus = Rect(int(self.cactusX - 5), int(self.cactusY - 5), 70, 70)
+        self.screen.blit(self.cactusImage, (self.cactusX, self.cactusY))
+        hitboxCactus = self.cactusImage.get_rect(
+            topleft=(self.cactusX, self.cactusY))
         pygame.draw.rect(self.screen, getRGB("red"), hitboxCactus, 2)
         return hitboxCactus
 
     def gameOver(self):
         # set font and text for gameover
         font = pygame.font.Font('freesansbold.ttf', 80)
-        text = font.render('Score: ' + str(self.score), True, getRGB('white'), None)
-        textRect = text.get_rect()
-        textRect.center = (800 // 2, 400 // 2)
-
-        # self.screen.fill(getRGB('white'))
+        text = font.render('Score: ' + str(self.score), True, getRGB('white'))
+        textRect = text.get_rect(center=(self.width // 2, self.height // 2))
         self.screen.blit(self.backgroundImage, (0, 0))
         self.screen.blit(text, textRect)
 
@@ -80,44 +85,32 @@ class Main:
         self.screen.blit(score, (700, 50))
 
     def collideDetect(self, rect1, rect2) -> bool:
-        """
-        :param rect1: rectangle 1
-        :param rect2: rectangle 2
-        :return: Boolean
-        """
-
         return rect1.colliderect(rect2)
 
     def gameLOOP(self):
         while self.run:
-            self.dinoY += 0.2
-            self.cactusX -= 0.25
+            self.clock.tick(60)
+            self.dinoY += 6 * self.adjustFrameFactor
+            self.cactusX -= min(30, max(10, self.score //
+                                10 + 1)) * self.adjustFrameFactor
 
-            if self.dinoX <= 0:
-                self.dinoX = 0
-            elif self.dinoX >= round(self.width - 64):
-                self.dinoX = round(self.width - 64)
-            elif self.dinoY <= 0:
-                self.dinoY = 0
-            elif self.dinoY >= round(self.height - 64):
-                self.dinoY = round(self.height - 64)
+            # Boundary conditions
+            if self.dinoY >= self.height - 64:
+                self.dinoY = self.height - 64
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.run = False
 
-                #   check if arrow keys are pressed
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         self.run = False
                     if event.key == pygame.K_SPACE:
                         self.dinoY -= 300
 
-            # self.screen.fill(getRGB("white"))
             self.screen.blit(self.backgroundImage, (0, 0))
 
             if not self.gameover:
-                #   fill the screen with white color
                 rect1 = self.dinosaur
                 rect2 = self.cactus()
 
@@ -132,9 +125,11 @@ class Main:
             else:
                 self.gameOver()
 
-            # updating the display
             pygame.display.update()
 
 
 if __name__ == "__main__":
-    Main().gameLOOP()
+    Main(
+        adjustFrameFactor=1.1
+    ).gameLOOP()
+    pygame.quit()
